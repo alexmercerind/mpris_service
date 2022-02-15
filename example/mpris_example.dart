1cbd639a1797c9f85d36b405a0994d12e65097a7
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-02-15 21:44:25 +0000

    Initial commit

---
+++ example/mpris_example.dart
@@ -0,0 +1,6 @@
+import 'package:mpris/mpris.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/mpris.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mpris;
+
+export 'src/mpris_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
