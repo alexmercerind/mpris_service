c0f58a532005336bf101a6966258eb1df854e6e8
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-02-16 08:09:58 +0000

    Add root .dart file

---
+++ lib/mpris_service.dart
@@ -0,0 +1,8 @@
+/// This file is a part of mpris_service.dart (https://github.com/alexmercerind/mpris_service.dart).
+///
+/// Copyright (c) 2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+/// All rights reserved.
+/// Use of this source code is governed by MIT license that can be found in the LICENSE file.
+library mpris_service;
+
+export 'package:mpris_service/src/mpris_service_base.dart';
