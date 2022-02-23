3435b3a5350c61bb16d3b4629d452d926684625c
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-02-23 18:48:53 +0000

    Add AGPL license

--- example/mpris_example.dart
+++ example/mpris_example.dart
@@ -1,8 +1,24 @@
+/// This file is a part of mpris_service.dart (https://github.com/harmonoid/mpris_service.dart).
+///
+/// Copyright (C) 2020-2022 Hitesh Kumar Saini <saini123hitesh@gmail.com>
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as
+/// published by the Free Software Foundation, either version 3 of the
+/// License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import 'package:mpris_service/mpris_service.dart';
 
 void main() {
-  late MPRISService mpris;
-  mpris = MPRISService(
+  MPRISService mpris = MPRISService(
     'harmonoid',
     identity: 'Harmonoid',
     desktopEntry: '/usr/share/applications/harmonoid.desktop',
--- lib/src/mpris_service.dart
+++ lib/src/mpris_service.dart
@@ -1,10 +1,19 @@
-/// This file is a part of mpris_service.dart (https://github.com/alexmercerind/mpris_service.dart).
+/// This file is a part of mpris_service.dart (https://github.com/harmonoid/mpris_service.dart).
 ///
-/// Copyright (c) 2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
-/// All rights reserved.
-/// Use of this source code is governed by MIT license that can be found in the LICENSE file.
-
-// ignore_for_file: non_constant_identifier_names
+/// Copyright (C) 2020-2022 Hitesh Kumar Saini <saini123hitesh@gmail.com>
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as
+/// published by the Free Software Foundation, either version 3 of the
+/// License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import 'dart:io';
 import 'dart:math';
